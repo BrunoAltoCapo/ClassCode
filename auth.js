7a86c0236edecffea92618bfdd5f9886aa0b067f
BrunoAltoCapo <brunete2001@gmail.com>
2025-03-31 14:29:12 +0200

    Funciona

--- auth.js
+++ auth.js
@@ -1,33 +1,43 @@
+// auth.js
+
+const scopes = [
+  'https://www.googleapis.com/auth/classroom.courses.readonly',
+  'https://www.googleapis.com/auth/classroom.coursework.me'
+];
+
+
 const { google } = require('googleapis');
-const { OAuth2Client } = require('google-auth-library');
-const {clientID, clientSecret} = require('.env');
+const OAuth2 = google.auth.OAuth2;
+require('dotenv').config(); // Carga las variables de entorno
+const clientID = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
 
-const oAuth2Client = new OAuth2Client(
-  clientID, // Sustituye con tu client ID
-  clientSecret, // Sustituye con tu client secret
-  'http://localhost:3000/oauth2callback' // URL de redirección
-);
+let oauth2Client; // Inicializa el cliente OAuth
 
-const SCOPES = ['https://www.googleapis.com/auth/classroom.courses.readonly'];
+// Función para obtener la URL de autorización
+const getAuthUrl = () => {
+  oauth2Client = new OAuth2(
+    process.env.CLIENT_ID,   // El CLIENT_ID que obtuviste de la consola de Google
+    process.env.CLIENT_SECRET,   // El CLIENT_SECRET que obtuviste de la consola de Google
+    'http://localhost:3000/oauth2callback'   // La URI de redirección que debe coincidir con la configuración en Google Cloud
+  );
 
-// Obtiene la URL para que el usuario inicie sesión y autorice la app
-function getAuthUrl() {
-  const authUrl = oAuth2Client.generateAuthUrl({
+  return oauth2Client.generateAuthUrl({
     access_type: 'offline',
-    scope: SCOPES,
+    scope: ['https://www.googleapis.com/auth/classroom.courses.readonly']
   });
-  return authUrl;
-}
+};
 
-// Intercambia el código por un token de acceso
-async function getTokens(code) {
-  const { tokens } = await oAuth2Client.getToken(code);
-  oAuth2Client.setCredentials(tokens);
+// Función para obtener los tokens
+const getTokens = async (code) => {
+  const { tokens } = await oauth2Client.getToken(code);
+  oauth2Client.setCredentials(tokens);
   return tokens;
-}
+};
 
+// Exporta las funciones
 module.exports = {
   getAuthUrl,
   getTokens,
-  oAuth2Client,
+  oAuth2Client: oauth2Client,
 };
